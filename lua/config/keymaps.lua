de0fac80a35d317d3f52cf521227271eb079d511
Edgardo Rodriguez <edgardorodriguezdev@gmail.com>
2025-09-30 09:27:02 -0600

    feat(color-schemes): add new themes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --#endregion
-
--- mapea shift shift para buscar files como en inteli
-vim.keymap.set("n", "<S-S>", "<cmd>Telescope find_files<CR>", { desc = "Buscar archivos (Shift Shift)" })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 return {
-  -- add gruvbox
+  -- list of themes
   { "ellisonleao/gruvbox.nvim" },
   { "rebelot/kanagawa.nvim" },
   { "vague2k/vague.nvim" },
+  { "sainnhe/everforest" },
+  { "olivercederborg/poimandres.nvim" },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load a theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vague",
+      colorscheme = "everforest",
     },
   },
 }
